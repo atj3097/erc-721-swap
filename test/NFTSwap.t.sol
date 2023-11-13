0b15f51dcf7f6753ce0e4944fe0bca3a5fb22cc9
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-12 21:48:01 -0500

    adding nft swap test

--- test/NFTSwap.t.sol
+++ test/NFTSwap.t.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "ds-test/test.sol";
+import "forge-std/Test.sol";
 import "../src/NFTSwap.sol";
 import "../src/NFTOne.sol";
 import "../src/NFTTwo.sol";
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-contract NFTSwapTest is DSTest {
+contract NFTSwapTest is Test {
     NFTSwap nftSwap;
     NFTOne nftOne;
     NFTTwo nftTwo;
@@ -37,18 +37,27 @@ contract NFTSwapTest is DSTest {
 
     function testCreateSwap() public {
         nftSwap.createSwap(address(nftOne), 1, address(nftTwo), 2, party1, party2);
-        Swap memory swap = nftSwap.swaps(0);
-        assertEq(swap.nftAddress1, address(nftOne));
-        assertEq(swap.tokenId1, 1);
-        assertEq(swap.nftAddress2, address(nftTwo));
-        assertEq(swap.tokenId2, 2);
-        assertEq(swap.party1, party1);
-        assertEq(swap.party2, party2);
+        (address nftAddress1, uint256 tokenId1, address nftAddress2, uint256 tokenId2, address party1, address party2, uint256 creationTime) = nftSwap.swaps(0);
+        assertEq(nftAddress1, address(nftOne));
+        assertEq(tokenId1, 1);
+        assertEq(nftAddress2, address(nftTwo));
+        assertEq(tokenId2, 2);
+        assertEq(party1, party1);
+        assertEq(party2, party2);
     }
 
     function testDepositNft() public {
         nftSwap.createSwap(address(nftOne), 1, address(nftTwo), 2, party1, party2);
+        vm.prank(party1);
+        nftOne.approve(address(nftSwap), 1);
+
+        vm.prank(party2);
+        nftTwo.approve(address(nftSwap), 2);
+
+        vm.prank(party1);
         nftSwap.depositNft(0, address(nftOne));
+
+        vm.prank(party2);
         nftSwap.depositNft(0, address(nftTwo));
         assertEq(nftSwap.deposited(party1), true);
         assertEq(nftSwap.deposited(party2), true);
